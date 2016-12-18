1b1fac9a91ec16e9e41ac763791c9968cd321765
Bobby Eickhoff <beickhoff@gmail.com>
2016-12-17 23:36:35 -0500

    Define additional colors and aliases; redraw the screen

--- demo/src/demo.cljs
+++ demo/src/demo.cljs
@@ -1,15 +1,17 @@
 (ns clojure-turtle.demo
   (:require [clojure-turtle.core :as turtle :include-macros true
-             :refer [clean color forward home left right setheading]]
-            [clojure-turtle.util :refer [black blue gray green orange purple red white yellow]]))
-
+             :refer [clean color forward home left right]]
+            [clojure-turtle.util
+             :refer [beige black blue brown dark-gray dark-green dark-red gray
+                     green light-cyan light-pink orange pen-down pen-up pink purple
+                     red set-heading straight white yellow]]))
 (defonce canvas
   (do
     (.log js/console "Creating turtle")
     (turtle/new-window {:size [650 700]})))
-
-(clean)
-(home)
+(do
+  (clean)
+  (home))
 
 ;(defn r-circle []
 ;  (doseq [i (range 360)]
